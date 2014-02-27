41c1271cb1f11126b49166e2fd4578f6c46f5c31
Georgiy <goshazvir@gmail.com>
2014-02-27 18:43:45 +0200

    load-2

--- js/common.js
+++ js/common.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-
+	function tab() {
+	     $(".js-tab").each(function(){
+	       var tab_link = $(this).find("a");
+	       var tab_item = $(this).find("li");
+	       var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+	       tab_cont.hide();
+	       tab_item.first().addClass("is-active");
+	       $(this).parents(".js-tab-group").find(".js-tab1").show();
+	       tab_link.on("click", function() {
+	           var index = $(this).attr("href");
+	           tab_item.removeClass("is-active");
+	           $(this).parent().addClass("is-active");
+	           tab_cont.hide();
+	           $(this).parents(".js-tab-group").find("."+index).show();
+	           return false;
+	        });
+	     });
+	}
+	tab();
 });
